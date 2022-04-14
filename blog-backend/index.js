88e0a43ac0f86e66848183d3eb08f4579e589b54
ophthalmologist <gusrylmubarok@gmail.com>
2022-04-14 20:29:46 +0700

    Fast Learn NodeJS

---
+++ blog-backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const productRoutes = require('./src/routes/products');
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use('/v1/customer', productRoutes);
+
+app.listen(4000);
+
